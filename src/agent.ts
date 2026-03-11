09892e64e0fd686796200c22ce5f76a3c5d17752
Srini5303 <srini.academics@gmail.com>
2026-03-11 14:26:32 -0400

    Restructure repo: include JobMatch-AI and JobApply projects

--- src/agent.ts
+++
@@ -1,5 +0,0 @@
-export { analyzeJDResume } from "./analyzers/jd_resume.ts";
-export { analyzeResumeStrength } from "./analyzers/resume.ts";
-export { generateCoverLetter } from "./analyzers/cover_letter.ts";
-export { searchAndRankJobs } from "./jobs/search.ts";
-export type { JobSearchPreferences, ResumeStrengthAnalysis, JobSearchResult } from "./types.ts";
